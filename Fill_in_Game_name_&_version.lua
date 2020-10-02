51f09bc2de1cde743a0ff5a66bbee0d9bce1defc
DhaosCollider <72247447+DhaosCollider@users.noreply.github.com>
2020-10-02 21:18:28 +0900

    Update Fill_in_Game_name_&_version.lua

--- Fill_in_Game_name_&_version.lua
+++ Fill_in_Game_name_&_version.lua
@@ -6,7 +6,7 @@ local _author = ''
 local _type   = 'aobscan'
 local _msg    = 'Author: '.._author..'\nError : '.._type..' '
 
-local Author = {[[
+local Author01 = {[[
 // Script_name
 aobscanmodule
 registersymbol
@@ -16,8 +16,26 @@ aobscanregion
 registersymbol
 ]]}
 
-for i = 1, #Author do
-  if autoAssemble(Author[i]) == false then print(_msg..Author[i]) end
+for i = 1, #Author01 do
+  if autoAssemble(Author01[i]) == false then print(_msg..Author01[i]) end
+end
+
+local _author = ''
+local _type   = 'aobscan'
+local _msg    = 'Author: '.._author..'\nError : '.._type..' '
+
+local Author02 = {[[
+// Script_name
+aobscanmodule
+registersymbol
+]],[[
+// Script_name
+aobscanregion
+registersymbol
+]]}
+
+for i = 1, #Author02 do
+  if autoAssemble(Author02[i]) == false then print(_msg..Author02[i]) end
 end
 -- {$asm}
 -- [DISABLE]
